8eb38e847d6d3e3267602fc3cc50dcca4fe5bd11
Iqbal Singh <Isingh1548@conestogac.on.ca>
2024-02-13 16:26:02 -0500

    Add project files.

---
+++ NUnit demonstration/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_demonstration
+{
+        public static class Triangle
+        {
+            public static string ValidTriangle(int firstAngle, int secondAngle, int
+            thirdAngle)
+            {
+                string result;
+            // Iqbal's Mutation test'
+                if ((firstAngle + secondAngle + thirdAngle) == 180)
+                {
+                    result = "The triangle is valid.";
+                }
+                else
+                {
+                    result = "The triangle is NOT valid.";
+                }
+                return result;
+            }
+        }
+    }
+
---
+++ Test class/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit_demonstration;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace Test_class
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+        public class TriangleTest2
+        {
+            [Test]
+            //Iqbal Triangle Test
+            public void ValidTriangle60()
+
+            {
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string Expected = "The triangle is valid.";
+
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(Expected, actual);
+            }
+        }
+
+
+    }
+}
